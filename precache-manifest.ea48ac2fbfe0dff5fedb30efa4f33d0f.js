e410efc06f6c8476434169f934524414fe8328e4
somashekhar dehury <somashekhar.dehury@gmail.com>
2022-07-18 19:53:22 +0530

    Updates

---
+++ precache-manifest.ea48ac2fbfe0dff5fedb30efa4f33d0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "44a40d624d30c290fcfb",
+    "url": "/cryptoproject/static/js/main.44a40d62.chunk.js"
+  },
+  {
+    "revision": "49779370604c73c661de",
+    "url": "/cryptoproject/static/css/1.d3434064.chunk.css"
+  },
+  {
+    "revision": "49779370604c73c661de",
+    "url": "/cryptoproject/static/js/1.49779370.chunk.js"
+  },
+  {
+    "revision": "64af5544fd96c2a5e540",
+    "url": "/cryptoproject/static/js/runtime~main.64af5544.js"
+  },
+  {
+    "revision": "885327321794d77a6b6f7ab7fd42d271",
+    "url": "/cryptoproject/static/media/cryptocurrency.88532732.png"
+  },
+  {
+    "revision": "5360311030cd1ec5d75eb51f4432c396",
+    "url": "/cryptoproject/index.html"
+  }
+];
